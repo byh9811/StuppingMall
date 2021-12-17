f0b2518b1a712c672f46301889152a45c5335c40
byh9811 <byh9811@naver.com>
2021-12-17 17:27:55 +0900

    StuppingMall 첫 커밋
    디펜던시 넣어서 프로젝트 만듦.
    기본 페이지 라우팅해놨음.

---
+++ stupping-mall/src/main/java/com/nerds/stuppingmall/StuppingMallApplication.java
@@ -0,0 +1,13 @@
+package com.nerds.stuppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class StuppingMallApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StuppingMallApplication.class, args);
+	}
+
+}
---
+++ stupping-mall/src/test/java/com/nerds/stuppingmall/StuppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerds.stuppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
