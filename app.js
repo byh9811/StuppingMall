14606e02d872f954147feafc3ef22aede483c26b
jeeeee_sung <85999976+jisung24@users.noreply.github.com>
2022-01-21 15:55:34 +0900

    nodejs이용해서 메인 로그인 마이페이지(미작업)까지 라우팅

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const myPageRouter = require("./myWorkFolder/routes/myPage");
+const mainPageRouter = require("./myWorkFolder/routes/mainPage");
+const authSignInRouter = require("./myWorkFolder/routes/authSiginIn");
+// /css/mypage.css
+app.use("/",mainPageRouter);
+app.use("/myPage",myPageRouter);
+app.use("/login",authSignInRouter);
+app.use(express.static('./myWorkFolder/public'));
+//전체 폴더에서 절대경로로 설정을 해줘야한다!!!
+
+app.set("views","./myWorkFolder/views/auth");
+app.set("view engine","ejs"); //ejs를 사용함!
+
+app.listen(3000,()=>{
+    console.log("3000번 서버!")
+})
--- myWorkFolder/routes/start.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router(); //router모듈 생성한다!!!
-
-router.get("/",(req,res)=>{
-    res.render("start.ejs");
-});
-
-
-module.exports = router;
