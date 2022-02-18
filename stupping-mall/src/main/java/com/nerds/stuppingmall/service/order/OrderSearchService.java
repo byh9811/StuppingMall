a2a60db9ec327ec7f53eba580d18dea2ee72e860
byh9811 <byh9811@naver.com>
2022-02-19 00:36:15 +0900

    주문 조회 쿼리 변경
    1. 내 주문내역 조회 쿼리를 변경함.
    2. 제조일자가 계속 사용되고 있던 부분을 고침.
    3. 다음은 myPick 기능을 개발하면 될듯.

--- stupping-mall/src/main/java/com/nerds/stuppingmall/service/order/OrderSearchService.java
+++ stupping-mall/src/main/java/com/nerds/stuppingmall/service/order/OrderSearchService.java
@@ -1,10 +1,17 @@
 package com.nerds.stuppingmall.service.order;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Service;
 
+import com.nerds.stuppingmall.domain.Notebook;
 import com.nerds.stuppingmall.domain.Order;
 import com.nerds.stuppingmall.repository.OrderRepository;
 
@@ -13,11 +20,18 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class OrderSearchService {
-	final OrderRepository orderRepository;
+	final MongoTemplate mongoTemplate;
 	final int SIZE_PER_PAGE = 10;
 	
 	public Page<Order> findMyOrders(int curPage, String id) {
 		Pageable pageable = PageRequest.of(curPage, 10);
-		return orderRepository.findAll(pageable);
+		Query query = new Query();
+		query.addCriteria(Criteria.where("_id").is(id));
+		query.with(pageable);
+
+		List<Order> myOrders = mongoTemplate.find(query, Order.class, "orders");
+		return PageableExecutionUtils.getPage(
+				myOrders, pageable,
+				() -> mongoTemplate.count(Query.of(query).limit(-1).skip(-1), Order.class));
 	}
 }
