e9be80de2799f5e308f558c5e8996079ac646faa
byh9811 <byh9811@naver.com>
2022-04-03 16:27:27 +0900

    신고 기능 개발
    1. 현재 신고된 report를 보여줄 페이지 생성
    2. 관리자가 report를 관리하는 컨트롤러 uri 개발

--- stupping-mall/src/main/java/com/nerds/stuppingmall/controller/AdminController.java
+++ stupping-mall/src/main/java/com/nerds/stuppingmall/controller/AdminController.java
@@ -19,13 +19,14 @@ import lombok.RequiredArgsConstructor;
 public class AdminController {
 	final MemberDeregisterService memberDeregisterService;
 	final MemberDetailsService memberDetailsService;
-	
+	final ReportSearchService reportSearchService;
+
 	@GetMapping("/allMembers")
 	public String allMemberList(Model model) {
-		Page<Member> memberPages = memberDetailsService.findAllMembers(0);
-		model.addAttribute("members", memberPages.getContent());
-		model.addAttribute("curPage", memberPages.getNumber());
-		model.addAttribute("maxPage", memberPages.getTotalPages());
+		Page<Member> memberPage = memberDetailsService.findAllMembers(0);
+		model.addAttribute("members", memberPage.getContent());
+		model.addAttribute("curPage", memberPage.getNumber());
+		model.addAttribute("maxPage", memberPage.getTotalPages());
 		return "memberInfo";
 	}
 
@@ -33,5 +34,14 @@ public class AdminController {
 	public String banMember(String id) {
 		memberDeregisterService.removeMember(id);
 		return "redirect:/admin/allMembers";
+	}
+
+	@GetMapping("/reports")
+	public String reportList(Model model) {
+		Page<Report> reportPage = reportSearchService.findAllReports();
+		model.addAttribute("reports", reportPage.getContent());
+		model.addAttribute("curPage", reportPage.getNumber());
+		model.addAttribute("maxPage", reportPage.getTotalPages());
+		return "report";
 	}
 }
