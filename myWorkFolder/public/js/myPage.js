1febc8585e0df51b963e4f142c6aa09154433004
jeeeee_sung <85999976+jisung24@users.noreply.github.com>
2022-05-01 15:13:00 +0900

    커밋 지금

--- myWorkFolder/public/js/myPage.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-$(window).on('scroll',function(){
-    if( $(window).scrollTop() > 120){
-        $('.sub_nav').addClass('nav_change');
-    }else{
-        $('.sub_nav').removeClass('nav_change');
-    }
-});
--- myWorkFolder/routes/myPage.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/",(req,res)=>{
-    res.render("myPage");
-});
-//dsdsdsdsssdsdsdsd pull req제대로 가나 봐봐
-module.exports = router;
