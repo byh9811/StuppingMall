aded88ff079d40d9f710230bdb23f5f8ed4f6984
byh9811 <byh9811@naver.com>
2022-07-02 17:53:34 +0900

    로그인 기능 변환
    1. css 갱신
    2. customerMain 카테고리 기능 submit 버튼 form 안으로 이동
    3. 로그인 페이지 타임리프로 변환

--- stupping-mall/src/main/java/com/nerds/stuppingmall/router/PasswordChangeWithoutAuthPageRouter.java
+++ stupping-mall/src/main/java/com/nerds/stuppingmall/router/PasswordChangeWithoutAuthPageRouter.java
@@ -8,7 +8,7 @@ import java.time.LocalTime;
 
 @Controller
 public class PasswordChangeWithoutAuthPageRouter {
-    @GetMapping("changePasswordWithoutLogin")
+    @GetMapping("/changePasswordWithoutLogin")
     public String changePasswordWithoutLogin(Model model) {
         model.addAttribute("date", LocalTime.now());
         return "common/changePasswordWithoutLogin";
